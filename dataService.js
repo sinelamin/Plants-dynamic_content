5a7a65820469d7e8f06bd3226de07d1900873b50
Alexey Mikhaylov <krummi1414@yandex.ru>
2023-10-10 22:58:02 +0300

    feat: add popup for service cards

--- dataService.js
+++ dataService.js
@@ -3,36 +3,42 @@ export const data = [
     service: "gardens",
     img: "./assets/service-card-1.png",
     title: "Garden care",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor nobis quas expedita nam fugiat, explicabo doloremque"
   },
   {
     service: "planting",
     img: "./assets/service-card-2.png",
     title: "Planting",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor nobis quas expedita nam fugiat, explicabo doloremque deleniti corporis quam alias illo iusto quis similique est vero voluptate! Officiis, at!"
   },
   {
     service: "lawn",
     img: "./assets/service-card-3.png",
     title: "Lawn care",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor nobis quas expedita nam fugiat, est vero voluptate! Officiis, at!"
   },
   {
     service: "planting",
     img: "./assets/service-card-4.png",
     title: "Planting",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor nobis quas expedita nam fugiat, explicabo doloremque deleniti corporis quam alias illo iusto quis similique est vero voluptate! Officiis, at, explicabo doloremque deleniti corporis quam alias illo iusto quis similique est vero voluptate! Officiis, at!"
   },
   {
     service: "gardens",
     img: "./assets/service-card-5.png",
     title: "Garden care",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi dolor nobis quas expedita nam fugiat!"
   },
   {
     service: "planting",
     img: "./assets/service-card-6.png",
     title: "Planting",
-    descr: "Lorem Ipsum has been the industry"
+    descr: "Lorem Ipsum has been the industry",
+    descrBig: "Sequi dolor nobis quas expedita nam fugiat, explicabo doloremque deleniti corporis quam alias illo iusto quis similique est vero voluptate! Officiis, at!"
   }
 ];
--- script.js
+++ script.js
@@ -27,7 +27,7 @@ burger.addEventListener('click', e => {
 });
 
 overlay.addEventListener('click', e => {
-  if (e.target.classList.contains('overlay')) {
+  if (menuList.classList.contains('menu--active') && e.target.classList.contains('overlay')) {
     e.preventDefault();
     hideBurger();
   }
@@ -171,6 +171,46 @@ function changeServiceCards(cards, arrDataAttribute) {
       card.classList.remove('card__out--focus');
     });
   }
+}
+
+//--------------------------------------------------------------------------------------
+
+//Service pop-up
+//--------------------------------------------------------------------------------------
+
+const popup = document.querySelector('.service-popup');
+const closePopup = document.querySelector('.service-popup__btn');
+const popupImg = document.querySelector('.service-popup__img');
+const popupTitle = document.querySelector('.service-popup__title');
+const popupDescr = document.querySelector('.service-popup__description');
+
+cards.forEach((card, index) => {
+  card.addEventListener('click', e => {
+    console.log(e.target);
+    console.log(index);
+    showPopUp(data, index);
+  });
+});
+
+closePopup.addEventListener('click', e => {
+  hienPopUp();
+});
+
+
+function showPopUp(data, index) {
+  popup.classList.add('service-popup--active');
+  popupImg.src = data[index].img;
+  popupTitle.innerText = data[index].title;
+  popupDescr.innerText = data[index].descrBig;
+  overlay.classList.add('overla--active');
+  overlay.style.height = document.documentElement.scrollHeight + 'px';
+  body.classList.add('body-menu--active');
+}
+
+function hienPopUp() {
+  popup.classList.remove('service-popup--active');
+  overlay.classList.remove('overla--active');
+  body.classList.remove('body-menu--active');
 }
 
 //--------------------------------------------------------------------------------------
